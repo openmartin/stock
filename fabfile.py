56fd394d1af1cebbf89e78e30eaf509595184956
openmartin <sunshinesmiler@gmail.com>
2014-04-05 18:36:05 +0800

    fabric auto deploy

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,16 @@
+from __future__ import with_statement
 from fabric.api import *
 
-env.hosts = ['ubuntu@ec2-54-254-116-230.ap-southeast-1.compute.amazonaws.com']
+env.hosts = ['ec2-54-254-116-230.ap-southeast-1.compute.amazonaws.com']
+env.user = 'ubuntu'
+env.key_filename = 'E:\\py_workspace\\zilikey.pem'
+env.activate = 'source ~/djenv/bin/activate'
 
 def test():
     local("./manage.py test my_app")
 
 def commit():
-    #local("git add -p && git commit")
-    pass
+    local("git add -p && git commit")
 
 def push():
     local("git push origin")
@@ -18,13 +21,12 @@ def prepare_deploy():
     push()
     
 def remote_pull():
-    run('cd djenv')
-    run('source bin/activate')
-    run('cd stock')
-    run('git pull')
+    with cd("djenv/stock"):
+        run("git pull")
 
 def remote_restart():
-    pass
+    run("kill -HUP `ps -ef | grep uwsgi | awk '($3==1){print $2}'`")
+    sudo("/etc/init.d/nginx restart")
 
 def deploy():
     prepare_deploy()
