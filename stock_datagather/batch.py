7803bd6b2b1b14a1fe5b4a9965f290aa29daa37d
openmartin <sunshinesmiler@gmail.com>
2014-04-07 01:07:04 +0800

    batch.py use today()

--- stock_datagather/batch.py
+++ stock_datagather/batch.py
@@ -9,7 +9,7 @@ from stock_datagather.day_increment import get_data
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stock_analysis.settings")
 
 if __name__ == '__main__':
-    trandt = date(2014, 4, 4)
+    trandt = date.today()
     end_date = trandt
     end_date_str = end_date.strftime('%Y%m%d')
     start_date = end_date
